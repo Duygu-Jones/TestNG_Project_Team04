aad835a580a343f4b7f4e40f4b8d7909e0169c88
Ibraka1 <ibraka48@gmail.com>
2023-12-11 17:38:23 +0300

    Ibrahim us17 tc01 test method eklendi aralık12

--- src/test/java/allovercommerce_com/tests/us17/TC01.java
+++ src/test/java/allovercommerce_com/tests/us17/TC01.java
@@ -1,4 +1,16 @@
 package allovercommerce_com.tests.us17;
 
+import allovercommerce_com.utilities.ConfigReader;
+import allovercommerce_com.utilities.Driver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
 public class TC01 {
+    @Test
+    public void testVendorBuy() {
+            Actions actions = new Actions(Driver.getDriver());
+            Driver.getDriver().get(ConfigReader.getProperty("URL"));
+
+
+    }
 }
