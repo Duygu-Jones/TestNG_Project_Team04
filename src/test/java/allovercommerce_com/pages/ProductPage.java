bd9f80519049d90da75906f728df215ff66b2aef
Duygu-Jones <duygujones@gmail.com>
2023-12-16 11:31:18 +0300

    product page edited

--- src/test/java/allovercommerce_com/pages/ProductPage.java
+++ src/test/java/allovercommerce_com/pages/ProductPage.java
@@ -2,14 +2,14 @@ package allovercommerce_com.pages;
 
 
 import allovercommerce_com.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 
 public class ProductPage {
-
-
-
-
 
     public ProductPage(){
         PageFactory.initElements(Driver.getDriver(), this);
@@ -118,9 +118,9 @@ public class ProductPage {
 
 
     //Duygu Jones (120)
-
-
-
+    @FindBy(xpath = "//a[@class='compare btn-product-icon']")                   public List<WebElement> addToCompareListDuygu;
+    @FindBy(xpath = "//a[@class='add_to_wishlist single_add_to_wishlist']")     public List<WebElement> addToWishlistListDuygu;
+    @FindBy(xpath = "//a[@class='wishlist block-type']")                        public WebElement wishlistButtonDuygu;
 
 
 
@@ -268,40 +268,40 @@ public class ProductPage {
 
 
     //Selma Aslan (270)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @FindBy(xpath = "//*[@class='login inline-type']")
+    public WebElement signIn;
+    @FindBy(xpath = "//*[@id='username']")
+    public WebElement userName1;
+    @FindBy(xpath = "(//*[@class='woocommerce-Input woocommerce-Input--text input-text'])[2]")
+    public WebElement password1;
+    @FindBy(xpath = "//button[@name='login']")
+    public WebElement signInol;
+    @FindBy(xpath = "//span[.='Sign Out']")
+    public WebElement signout1;
+    @FindBy(xpath = "//a[.='Addresses']")
+    public WebElement address;
+    @FindBy(xpath = "(//*[@class='edit btn btn-link btn-primary btn-underline mb-4'])[2]")
+    public WebElement add;
+    @FindBy(xpath = "//*[@id='shipping_first_name']")
+    public WebElement firstName;
+    @FindBy(xpath = "//*[@id='shipping_last_name']")
+    public WebElement lastName;
+    @FindBy(xpath = "//*[@id='shipping_company']")
+    public WebElement company;
+    @FindBy(xpath = "//select[@data-placeholder='Select a country / region…']")
+    public WebElement country1;
+    @FindBy(xpath = "//*[@name='shipping_address_1']")
+    public WebElement streetAddress1;
+    @FindBy(xpath = "//*[@name='shipping_address_2']")
+    public WebElement streetAddress2;
+    @FindBy(xpath = "//*[@name='shipping_city']")
+    public WebElement city;
+    @FindBy(xpath = "//select[@data-placeholder='Select an option…']")
+    public WebElement state;
+    @FindBy(xpath = "//input[@name='shipping_postcode']")
+    public WebElement zipCode;
+    @FindBy(xpath = "//*[@class='btn btn-dark btn-rounded btn-sm']")
+    public WebElement saveAddress;
 
 
 
@@ -368,6 +368,56 @@ public class ProductPage {
 
 
     //Ibrahim Akar (370)
+    @FindBy(xpath = "//*[@name='add-to-cart']")
+    public WebElement addToCartIbrahim;
+
+    @FindBy(xpath = "//i[@class='w-icon-cart']")
+    public WebElement cartSymolIbrahim;
+
+    @FindBy(xpath = "//a[@class ='button checkout wc-forward']")
+    public WebElement checkOutButtonIbrahim;
+
+    @FindBy(xpath = "//tr//td[@class='product-name']")
+    public WebElement productListIbrahim;
+
+    @FindBy(xpath = "(//bdi)[6]")
+    public WebElement totalAmountIbrahim;
+
+    @FindBy(xpath = "(//tr//td[@class='product-name'])[1]")
+    public WebElement firstProduct;
+
+    @FindBy(xpath = "(//tr//td[@class='product-name'])[2]")
+    public WebElement secondProduct;
+
+    @FindBy(xpath = "//*[@class='woocommerce-message alert alert-simple alert-btn alert-success']")
+    public WebElement addToCartConfirm;
+
+    @FindBy(xpath = "//*[@class='cart-item-meta mini-item-meta']")
+    public WebElement cartDisplay;
+
+    @FindBy(xpath = "//*[@class='button wc-forward']")
+    public WebElement viewCart;
+
+    @FindBy(xpath = "//*[@class='quantity-plus w-icon-plus']")
+    public WebElement productPlus;
+
+    @FindBy(name = "update_cart")
+    public WebElement updateCart;
+
+    @FindBy(xpath = "//*[@class='input-text qty text']")
+    public WebElement productQuantity;
+
+    @FindBy(xpath = "//button[@class='quantity-minus w-icon-minus']")
+    public WebElement productMinus;
+
+    @FindBy(id ="coupon_code" )
+    public WebElement couponBox;
+
+    @FindBy(xpath = "//*[@class='cart-discount coupon-dsc18904']")
+    public WebElement couponAddMessage;
+
+    @FindBy(xpath = "//*[@class='checkout-button button alt wc-forward']")
+    public WebElement proceedToCheckOut;
 
 
 
